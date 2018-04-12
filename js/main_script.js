8aec58c9813c1cd75720dda67541f59b42b04441
jerrygoyal <1gouravgg@gmail.com>
2018-04-13 01:53:37 +0530

    added about-me intro

--- js/main_script.js
+++ js/main_script.js
@@ -6,7 +6,62 @@ $(document).ready(function () {
     if (window.location.hash) {
         scrollTo(window.location.hash);
     }
+
+        var typewrite_cl = '.typewrite';
+    if($(typewrite_cl).length>0){
+        animateTypewrite(typewrite_cl);
+    }
 });
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 3; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+function animateTypewrite(typewrite_cl){
+    var elements = $(typewrite_cl);
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+}
 function scrollTo(hash) {
     var stickyNavHeight = 0;
     if ($(hash).length > 0) {
